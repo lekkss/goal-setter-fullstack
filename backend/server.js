fdf71cc1c801a7c3c8edbefc41636d9565a768b4
lekkss <leksguy97@gmail.com>
2022-11-07 22:26:18 +0100

    add mongodb

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
+const colors = require("colors");
 const dotenv = require("dotenv").config();
 const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDb = require("./config/db");
 const port = process.env.PORT || 5000;
 const goalRoute = require("./routes/goalRoutes");
 
+connectDb();
 const app = express();
 
 app.use(express.json());
