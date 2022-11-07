1af1255e7a63ecd8b1336e9af78384ed5278a331
lekkss <leksguy97@gmail.com>
2022-11-07 21:46:10 +0100

    body middleware

--- backend/server.js
+++ backend/server.js
@@ -5,8 +5,8 @@ const goalRoute = require("./routes/goalRoutes");
 
 const app = express();
 
-// app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 app.use("/api/goals", goalRoute);
 
